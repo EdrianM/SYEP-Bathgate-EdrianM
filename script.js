4cc63fe6cd216bb544d34e4ceec2fb3ce7ac8d99
Your Name <your-email@example.com>
2025-08-11 01:06:00 +0000

    added testimonials

--- script.js
+++ script.js
@@ -1,28 +1,26 @@
 const reviews = [
-    {
-        id:1,
-        name: 'Eve Abraha',
-        job:'Physics Teacher',
-        img: 'ea.png',
-        text: ' Edrian, you always complete your work on time, ask questions in class and support your classmates. Keep up the good work, you rarely procrastinate and that is one of your strongest assets.',
-    },
-    {
-        id:2,
-        name: 'Alain Latoritue',
-        job:'ELA Teacher',
-        img: 'https://media.licdn.com/dms/image/v2/C4D03AQH1hb_YiDgvkQ/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1662519432541?e=1757548800&v=beta&t=csTq3sDHWWTAMvluGBhVvMtwLDXqglXVEEfG9WXer2g',
-        text: 'You were so much fun to teach and really appreciate the positive energy you bring to the class. Along with that energy, you always showed to me and to your peers how bright you are.',
-        
-    },
-    {
-        id:3,
-        name: 'Peter Jones',
-        job:'web developer',
-        img: 'https://via.placeholder.com/150',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto asperiores debitis incidunt, eius earum ipsam cupiditate libero? Iste, doloremque nihil?',
-    },
+  {
+    id: 1,
+    name: 'Eve Abraha',
+    job: 'Physics Teacher',
+    img: 'ea.png',
+    text: 'Edrian, you always complete your work on time, ask questions in class and support your classmates. You rarely procrastinate and that is one of your strongest assets.',
+  },
+  {
+    id: 2,
+    name: 'Alain Latortue',
+    job: 'Ela Teacher',
+    img: 'https://media.licdn.com/dms/image/v2/C4D03AQH1hb_YiDgvkQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1662519432541?e=1757548800&v=beta&t=dWbGu_R7hN520baUk1mb3oWycDN6pFQmicuewjc2cbc',
+    text: 'You were so much fun to teach and really appreciate the positive energy you bring to the class. Along with that energy, you always showed to me and to your peers how bright you are.',
+  },
+  {
+    id: 3,
+    name: 'Lisbel Feliz',
+    job: 'Math Teacher',
+    img: 'https://media.licdn.com/dms/image/v2/C4D03AQHiX_arQbo5VQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1619111322050?e=1757548800&v=beta&t=6N8LtxPBW_cuIGTee1tKEeSXF34CqKPdnZO04EqIN0o',
+    text: 'Edrian is a very fast learner and is very proactive when learning new material. He is also very helpful to his peers.',
+  },
 ];
-
 // select items
 const img = document.getElementById('person-img');
 const author = document.getElementById('author');
@@ -31,38 +29,44 @@ const info = document.getElementById('info');
 
 const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
 
+// set starting item
 let currentItem = 0;
 
 // load initial item
-window.addEventListener('DOMContentLoaded', function(){
-    showPerson(currentItem);
+window.addEventListener('DOMContentLoaded', function () {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
 });
 
-function showPerson(person){
-    const item = reviews[person];
-    img.src = item.img;
-    author.textContent = item.name;
-    job.textContent = item.job;
-    info.textContent = item.text;
+// show person based on item
+function showPerson(person) {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
 }
-
-nextBtn.addEventListener('click', function(){
-    currentItem++;
-    if(currentItem > reviews.length - 1) {
-        currentItem = 0;
-    }
-    showPerson(currentItem);
+// show next person
+nextBtn.addEventListener('click', function () {
+  currentItem++;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
 });
-
-prevBtn.addEventListener('click', function(){
-    currentItem--;
-    if(currentItem < 0) {
-        currentItem = reviews.length - 1;
-    }
-    showPerson(currentItem)
+// show prev person
+prevBtn.addEventListener('click', function () {
+  currentItem--;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
 });
-
 
 
 
